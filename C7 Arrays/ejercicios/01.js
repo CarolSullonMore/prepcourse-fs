16122b6296aafd2bc9b111271f82a7fc334f8722
CarolSullonMore <alidraeckart@gmail.com>
2024-10-17 00:09:43 -0500

    VT Website

--- C7 Arrays/ejercicios/01.js
+++ C7 Arrays/ejercicios/01.js
@@ -1,9 +1,7 @@
 function devolverPrimerElemento(array) {
   // Retornar el primer elemento del arreglo recibido.
   // Tu código:
-  let primerElemento = [10, 16, 12], array => 1 {
-    return primerElemento; 
-  });
+  return array[0]; 
 }
 
 module.exports = devolverPrimerElemento;
--- C7 Arrays/ejercicios/07.js
+++ C7 Arrays/ejercicios/07.js
@@ -2,6 +2,11 @@ function ordenarArray(array) {
   // Ordena los elementos del areglo array de menor a mayor.
   // Devuelve el arreglo resultante.
   // Tu código:
+  array.sort((a, b) => {
+  return a.localeCompare(b); // Compara cadenas alfabéticamente
+});
+
+  return array;
 }
 
 module.exports = ordenarArray;
