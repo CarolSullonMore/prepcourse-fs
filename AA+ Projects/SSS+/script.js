56ac9812075fe3dcb0f85a53f64a21aab6d96e49
CarolSullonMore <alidraeckart@gmail.com>
2024-10-22 23:37:12 -0500

    Nav DONE almost

---
+++ AA+ Projects/SSS+/script.js
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('toggle-btn');
+const sidebar = document.getElementById('sidebar');
+
+function toggleSideBar(){
+    sidebar.classList.toggle('close');
+    toggleButton.classList.toggle('rotate');
+};
+
+function toggleSubMenu(button){
+    button.nextElementSibling.classList.toggle('show');
+    button.classList.toggle('rotate');
+};
+
